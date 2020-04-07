9967188b325961c036c9b553a1d8b2b77b4163bf
buybackoff <vbaybekov@gmail.com>
2020-04-07 20:48:40 +0200

    Tesk MKL and high-level call in CI

--- test/Spreads.BLAS.Tests/OpenBLASTests.cs
+++ test/Spreads.BLAS.Tests/OpenBLASTests.cs
@@ -25,17 +25,32 @@ namespace Spreads.Tests
             Console.WriteLine(OpenBLAS.OpenblasGetNumProcs());
         }
 
-        [Test, Explicit("depends on MKL")]
+        [Test]
         public void MklCallsWork()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 Assert.IsTrue(MKL.MKL_GetMaxThreads() == Environment.ProcessorCount || MKL.MKL_GetMaxThreads() == Environment.ProcessorCount / 2);
             }
-            else
+        }
+
+        [Test]
+        public unsafe void CouldCallHiLevelSgemm()
+        {
+            var mnk = 2;
+            var x = new float[mnk * mnk];
+
+            for (int i = 0; i < mnk * mnk; i++)
             {
-                Assert.Inconclusive();
+                x[i] = (i + 1);
             }
+
+            var h = x.AsMemory().Pin();
+            var c = new float[mnk * mnk];
+            var hc = c.AsMemory().Pin();
+
+            CBLAS.Sgemm(LAYOUT.RowMajor, TRANSPOSE.NoTrans, TRANSPOSE.NoTrans,
+                mnk, mnk, mnk, alpha: 1f, (float*) h.Pointer, mnk, (float*) h.Pointer, mnk, beta: 0, (float*) hc.Pointer, mnk);
         }
 
         [Test, Explicit("Bench")]
@@ -108,73 +123,6 @@ namespace Spreads.Tests
             }
 
             Benchmark.Dump();
-        }
-
-        [Test, Explicit("Bench")]
-        public unsafe void CouldCallSgemm()
-        {
-            var mnk = 2;
-            var count = 100;
-            var rounds = 5;
-            var x = new float[mnk * mnk];
-
-            for (int i = 0; i < mnk * mnk; i++)
-            {
-                x[i] = (i + 1);
-            }
-
-            OpenBLAS.OpenblasSetNumThreads(1);
-            MKL.MKL_SetThreadingLayer(1);
-            MathNet.Numerics.Control.UseSingleThread();
-            var nd = np.arange(mnk * mnk).reshape(mnk, mnk);
-
-            var h = x.AsMemory().Pin();
-            var c = new float[mnk * mnk];
-            var hc = c.AsMemory().Pin();
-
-            var mdnX = MathNet.Numerics.LinearAlgebra.Matrix<float>.Build.Dense(mnk, mnk, x);
-            var mdnC = MathNet.Numerics.LinearAlgebra.Matrix<float>.Build.Dense(mnk, mnk);
-
-            for (int _ = 0; _ < rounds; _++)
-            {
-                using (Benchmark.Run($"OB__Sgemm_{mnk}", count * 2L * mnk * mnk * mnk))
-                {
-                    for (int i = 0; i < count; i++)
-                    {
-                        OpenBLAS.CBLAS.Sgemm(LAYOUT.RowMajor, TRANSPOSE.NoTrans, TRANSPOSE.NoTrans,
-                            mnk, mnk, mnk, alpha: 1f, (float*) h.Pointer, mnk, (float*) h.Pointer, mnk, beta: 0, (float*) hc.Pointer, mnk);
-                    }
-                }
-
-                using (Benchmark.Run($"MKL_Sgemm_{mnk}", count * 2L * mnk * mnk * mnk))
-                {
-                    for (int i = 0; i < count; i++)
-                    {
-                        MKL.CBLAS.Sgemm(LAYOUT.RowMajor, TRANSPOSE.NoTrans, TRANSPOSE.NoTrans,
-                            mnk, mnk, mnk, alpha: 1f, (float*) h.Pointer, mnk, (float*) h.Pointer, mnk, beta: 0, (float*) hc.Pointer, mnk);
-                    }
-                }
-
-                GC.Collect(2, GCCollectionMode.Forced);
-                using (Benchmark.Run($"M.N_Sgemm_{mnk}", (count / 50) * 2L * mnk * mnk * mnk))
-                {
-                    for (int i = 0; i < count / 50; i++)
-                    {
-                        mdnX.Multiply(mdnX, mdnC);
-                    }
-                }
-
-                GC.Collect(2, GCCollectionMode.Forced);
-                using (Benchmark.Run($"N#_Sgemm_{mnk}", (count / 50) * 2L * mnk * mnk * mnk))
-                {
-                    for (int i = 0; i < count / 50; i++)
-                    {
-                        np.matmul(nd, nd);
-                    }
-                }
-
-                GC.Collect(2, GCCollectionMode.Forced);
-            }
         }
     }
 }
